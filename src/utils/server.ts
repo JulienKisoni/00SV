d1684d0c163956796fa923a2873c713a3ba03b5e
Julien Kisoni <julienkisoni@bunid.ca>
2024-11-07 19:32:45 -0500

    add load testing

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -27,14 +27,18 @@ export const startServer = async (port: string, app: Application): Promise<http.
   function onListening() {
     const addr = server.address();
     const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr?.port}`;
-    if (process.env.TEST_ENABLED !== 'true') {
-      console.info(`Server is listening on ${bind}`);
-    }
+    // if (process.env.TEST_ENABLED !== 'true') {
+    console.info(`Server is listening on ${bind}`);
+    // }
   }
   const DATABASE_URI = process.env.DATABASE_URI;
   let DATABASE_NAME = process.env.DATABASE_NAME;
   if (process.env.TEST_ENABLED && process.env.TEST_ENABLED === 'true') {
-    DATABASE_NAME = process.env.DATABASE_TEST_NAME;
+    if (process.env.LOAD_TEST_ENABLED === 'true') {
+      DATABASE_NAME = process.env.DATABASE_LOAD_TEST_NAME;
+    } else {
+      DATABASE_NAME = process.env.DATABASE_TEST_NAME;
+    }
   }
   const errMsg = 'Could not connect to DB';
   if (!DATABASE_URI || !DATABASE_NAME) {
@@ -44,9 +48,7 @@ export const startServer = async (port: string, app: Application): Promise<http.
   const connection_uri = `${DATABASE_URI}/${DATABASE_NAME}`;
   try {
     await connect(connection_uri);
-    if (process.env.TEST_ENABLED !== 'true') {
-      console.log(`Connected to DB ${DATABASE_NAME}`);
-    }
+    console.log(`Connected to DB ${DATABASE_NAME}`);
     server.listen(port);
     server.on('error', onError);
     server.on('listening', onListening);
